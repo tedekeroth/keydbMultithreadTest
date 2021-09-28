691fdd6903421e923a329cc54891e1cba0810aac
tedekeroth <ted.ekeroth@gmail.com>
2021-09-28 11:41:43 +0200

    Add files via upload

---
+++ KeyDbMultithreadTest/KeyDbMultithreadTest/ChildTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyDbMultithreadTest
+{
+    public class ChildTest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Address { get; set; }
+        public List<string> DataStrings { get; set; }
+    }
+}
